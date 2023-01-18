462a23d7e72b989190ad6d381470ecc365e89d22
Luke Kreczko <lkreczko@googlemail.com>
2023-01-18 17:55:29 +0000

    feat: add list_python_packages_with_versions

--- src/fasthep_carpenter/utils.py
+++ src/fasthep_carpenter/utils.py
@@ -4,3 +4,12 @@ from pathlib import Path
 
 def mkdir_p(path: Path | str) -> None:
     os.makedirs(path, exist_ok=True)
+
+def list_python_packages_with_versions() -> dict[str, str]:
+    import pkg_resources
+
+    return {
+        dist.project_name: dist.version
+        for dist in pkg_resources.working_set
+        if dist.project_name != "pip"
+    }
