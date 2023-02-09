85855ddfb0b384485598318478c9cc5edcd1e31f
kreczko <lkreczko@googlemail.com>
2023-02-09 15:54:39 +0000

    feat: add register and unregister functions for collections

--- src/fasthep_carpenter/utils.py
+++ src/fasthep_carpenter/utils.py
@@ -1,9 +1,11 @@
 import os
 from pathlib import Path
+from typing import Any, Dict
 
 
 def mkdir_p(path: Path | str) -> None:
     os.makedirs(path, exist_ok=True)
+
 
 def list_python_packages_with_versions() -> dict[str, str]:
     import pkg_resources
@@ -13,3 +15,19 @@ def list_python_packages_with_versions() -> dict[str, str]:
         for dist in pkg_resources.working_set
         if dist.project_name != "pip"
     }
+
+
+def register_in_collection(
+    collection: Dict[str, Any], collection_name: str, name: str, obj: Any
+) -> None:
+    if name in collection:
+        raise ValueError(f"{collection_name} {name} already registered.")
+    collection[name] = obj
+
+
+def unregister_from_collection(
+    collection: Dict[str, Any], collection_name: str, name: str, obj: Any
+) -> None:
+    if name not in collection:
+        raise ValueError(f"{collection_name} {name} not registered.")
+    collection[name].pop()
