cd6f95ebcdb1fc1085523e2928fdea6e6d2b0c14
kreczko <lkreczko@googlemail.com>
2023-03-16 17:03:46 +0000

    refactor: split main function into main (CLI) + run(from within python)

--- src/fasthep_carpenter/main.py
+++ src/fasthep_carpenter/main.py
@@ -10,31 +10,16 @@ from .utils import mkdir_p
 app = typer.Typer()
 
 
-@app.command()
-def main(
-    dataset_cfg: Path = typer.Argument(
-        ...,
-        help="Dataset config to run over",
-        exists=True,
-        readable=True,
-        resolve_path=True,
-    ),
-    sequence_cfg: Path = typer.Argument(
-        ...,
-        help="Config for how to process dataset",
-        exists=True,
-        readable=True,
-    ),
-    output_dir: str = typer.Option(
-        "output", "--outdir", "-o", help="Where to save the results"
-    ),
-    processing_backend: str = typer.Option(
-        "multiprocessing", "--backend", "-b", help="Backend to use for processing"
-    ),
-    store_bookkeeping: bool = typer.Option(
-        True, "--store-bookkeeping", "-s", help="Store bookkeeping information"
-    ),
-):
+def run(dataset_cfg: Path, sequence_cfg: Path, output_dir: str, processing_backend: str, store_bookkeeping: bool):
+    """Entry point for running fasthep_carpenter from python
+
+    Args:
+        dataset_cfg (Path): Dataset config to run over
+        sequence_cfg (Path): Config for how to process dataset
+        output_dir (str): Where to save the results
+        processing_backend (str): Backend to use for processing
+        store_bookkeeping (bool): Store bookkeeping information
+    """
     try:
         import fast_curator
         import fast_flow.v1
@@ -44,11 +29,10 @@ def main(
     except ImportError as e:
         rich.print("[red]Failed to import required package:[/red]", e)
         sys.exit(1)
-
     sequence, sequence_cfg = fast_flow.v1.read_sequence_yaml(
         sequence_cfg,
         output_dir=output_dir,
-        backend="fast_carpenter",
+        backend="fasthep_carpenter",
         return_cfg=True,
     )
     datasets = fast_curator.read.from_yaml(dataset_cfg)
@@ -74,6 +58,35 @@ def main(
     )
     rich.print(f"[blue]Results[/]: {results}")
     rich.print(f"[blue]Output written to directory {output_dir}[/]")
+
+
+@app.command()
+def main(
+    dataset_cfg: Path = typer.Argument(
+        ...,
+        help="Dataset config to run over",
+        exists=True,
+        readable=True,
+        resolve_path=True,
+    ),
+    sequence_cfg: Path = typer.Argument(
+        ...,
+        help="Config for how to process dataset",
+        exists=True,
+        readable=True,
+    ),
+    output_dir: str = typer.Option(
+        "output", "--outdir", "-o", help="Where to save the results"
+    ),
+    processing_backend: str = typer.Option(
+        "multiprocessing", "--backend", "-b", help="Backend to use for processing"
+    ),
+    store_bookkeeping: bool = typer.Option(
+        True, "--store-bookkeeping", "-s", help="Store bookkeeping information"
+    ),
+):
+    """Entry point for running fasthep_carpenter from command line"""
+    run(dataset_cfg, sequence_cfg, output_dir, processing_backend, store_bookkeeping)
 
 
 if __name__ == "__main__":
