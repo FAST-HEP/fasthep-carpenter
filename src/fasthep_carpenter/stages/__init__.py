a354db5b9c15152d1210ebe83d56fdb04845a3b1
kreczko <lkreczko@googlemail.com>
2023-04-18 15:54:56 +0100

    feat: add draft for DataSource stage

--- src/fasthep_carpenter/stages/__init__.py
+++ src/fasthep_carpenter/stages/__init__.py
@@ -1,7 +1,9 @@
+from .data_source import DataSource
 from .define import Define
 from .output import ConsoleOutput, FileOutput
 
 __all__ = [
+    "DataSource",
     "Define",
     "ConsoleOutput",
     "FileOutput",
