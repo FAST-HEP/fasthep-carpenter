0157005a43f4669fe2629b8ef0eefb78ff7451ed
kreczko <lkreczko@googlemail.com>
2023-04-24 16:46:32 +0100

    feat: add stage for calculating invariant mass

--- src/fasthep_carpenter/stages/__init__.py
+++ src/fasthep_carpenter/stages/__init__.py
@@ -1,10 +1,12 @@
 from .data_source import DataSource
 from .define import Define
+from .invariantMass import InvariantMass
 from .output import ConsoleOutput, FileOutput
 
 __all__ = [
+    "ConsoleOutput",
     "DataSource",
     "Define",
-    "ConsoleOutput",
     "FileOutput",
+    "InvariantMass",
 ]
