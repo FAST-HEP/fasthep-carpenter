9688aadfc0fc2cb68db0d923417f2a9a6781deff
kreczko <lkreczko@googlemail.com>
2023-06-13 16:34:45 +0100

    feat: first working version of histogram filling

--- src/fasthep_carpenter/stages/histogram.py
+++ src/fasthep_carpenter/stages/histogram.py
@@ -1,32 +1,41 @@
-
 from typing import Any
 
 from fasthep_logging import get_logger
 
-from ..protocols import DataMapping, ProcessingStep, ProcessingStepResult
+from ..protocols import ProcessingStep, ProcessingStepResult
+from ..utils import broadcast_weights, flatten_and_remove_none
 
 log = get_logger("FASTHEP::Carpenter")
 
 
 class Histogram(ProcessingStep):
-
     _name: str
     _prefix: str = "__histogram__"
     _binning: dict[str, tuple[float, float, int]]
-    _weights: dict[str, str]
+    _weights: dict[str, str] | None
     multiplex: bool = True
 
-    def __init__(self, name: str, binning: dict[str, tuple[Any]], weights: dict[str, str] = None, **kwargs) -> None:
+    def __init__(
+        self,
+        name: str,
+        binning: dict[str, tuple[float, float, int]],
+        weights: dict[str, str] | None = None,
+        **kwargs,
+    ) -> None:
         self._name = name
         self._binning = binning
         self._weights = weights
 
     def __call__(self, data: ProcessingStepResult) -> ProcessingStepResult:
         import awkward as ak
+
         log.trace(f"Processing stage {self._name}")
-        data.bookkeeping[(self.__class__.__name__, self._name)] = self.__dask_tokenize__()
+        data.bookkeeping[
+            (self.__class__.__name__, self._name)
+        ] = self.__dask_tokenize__()
 
         import hist
+
         axes = []
         for name, (bins, low, high) in self._binning.items():
             axes.append(hist.axis.Regular(bins, low, high, name=name))
@@ -37,18 +46,15 @@ class Histogram(ProcessingStep):
         arrays = data.data.arrays(list(self._binning.keys()), how=dict)
 
         # extend weights to the same length as the data
-        weight = data.data[self._weights["weighted"]]
-        weights = {}
-        for name, value in arrays.items():
-            weights[name] = ak.flatten(ak.broadcast_arrays(weight, value)[0])
-        arrays = {k: ak.flatten(v, axis=1) for k, v in arrays.items()}
-        # remove empty entries
-        arrays = {k: v[~ak.is_none(v)] for k, v in arrays.items()}
-        weights = {k: v[~ak.is_none(v)] for k, v in weights.items()}
+        weight_values = broadcast_weights(
+            arrays, data.data.arrays(self._weights, how=dict)
+        )
+        # remove empty entries and flatten
+        arrays = flatten_and_remove_none(arrays)
+        weight_values = flatten_and_remove_none(weight_values)
+        weight = weight_values.popitem()[1]
 
-        weight_values = weights.popitem()[1]
-
-        histogram.fill(**arrays, weight=weight_values)
+        histogram.fill(**arrays, weight=weight)
         if data.result is None:
             data.result = {}
         data.result[self.name] = histogram
